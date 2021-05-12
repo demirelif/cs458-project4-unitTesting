60c15e6c70c8076eb907666f7d4aa035ebbf67f2
elif.demir <ed.demirelif@gmail.com>
2021-05-12 21:58:58 +0300

    add symptom test

--- backend/src/test/java/com/example/demo/patient/PatientTest.java
+++ backend/src/test/java/com/example/demo/patient/PatientTest.java
@@ -1,24 +1,36 @@
 package com.example.demo.patient;
 
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PatientTest {
 
+    @Mock
+    Patient patient;
+
     @Test
     void addSymptom() {
-    }
-
-    @Test
-    void removeSymptom() {
-    }
-
-    @Test
-    void getSymptoms() {
-    }
-
-    @Test
-    void setSymptoms() {
+        JSONObject patientJSON = new JSONObject();
+        patientJSON.put("name", "elif");
+        patientJSON.put("age", 22);
+        patientJSON.put("surname", "nn");
+        patientJSON.put("email", "elifd@mail.com");
+        patientJSON.put("password", "1234");
+        patientJSON.put("gender", "f");
+        patientJSON.put("gender", "f");
+        patient = new Patient(patientJSON);
+        Set<Symptom> symptoms = new HashSet<>();
+        symptoms.add(Symptom.FEVER);
+        symptoms.add(Symptom.NAUSEA);
+        patient.addSymptom(new DailySymptom("03.04.2021", symptoms));
+        assertEquals(true,true);
     }
 }
