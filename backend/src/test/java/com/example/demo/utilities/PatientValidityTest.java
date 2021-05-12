0972e5b3e952b325cf1ace9fc434b480b0fd7dfb
elif.demir <ed.demirelif@gmail.com>
2021-05-12 20:10:06 +0300

    patient validity test

---
+++ backend/src/test/java/com/example/demo/utilities/PatientValidityTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.utilities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientValidityTest {
+
+    @Test
+    void checkPatientValidity() {
+    }
+
+    @Test
+    void checkEmail() {
+    }
+}
