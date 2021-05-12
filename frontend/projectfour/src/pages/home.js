b005157c6caf6ee73caa3a97d95e30374b70b61a
Artun <curaartun@gmail.cm>
2021-05-12 16:20:27 +0300

    Other things

--- frontend/projectfour/src/pages/home.js
+++ frontend/projectfour/src/pages/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './home.css'
 import CovidImage from '../static/cvd19.jpg'
+import { alignAuto } from 'react-charts/dist/react-charts.development';
   
 const Home = () => {
     return(
       <>
-      <img src={CovidImage}/>
+      <img src={CovidImage} className="image"/>
       <h1 className="welcome-text">Welcome to COVID Tracking Portal</h1>
       </>
     )
