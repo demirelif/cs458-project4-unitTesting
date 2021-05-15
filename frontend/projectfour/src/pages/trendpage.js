9f5bfdafff75a32dc418192fff15521ce69f1ae1
Artun <curaartun@gmail.cm>
2021-05-15 15:07:22 +0300

    trendpage fix

--- frontend/projectfour/src/pages/trendpage.js
+++ frontend/projectfour/src/pages/trendpage.js
@@ -13,17 +13,29 @@ const TrendPage = () => {
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [modalText, setModalText] = useState("");
 
-  const graphdata = [{x:0,y:0},{x:1,y:3},{x:2,y:6},{x:3,y:2}]
+  const [graphdata, setGraphdata] = useState([])
 
   const data = React.useMemo(
-    () => [
-      {
-        label: 'Your trends',
-        data: graphdata
-      }
-    ],
-    []
+    () =>
+      [
+        {
+          label: 'Your trends',
+          data: (graphdata.length) < 1 ? [] : graphdata
+        }
+      ],
+    [graphdata]
   )
+
+  useEffect(() => {
+    //async bi request at, bekle
+    axios.get(`http://localhost:8080/api/patient/symptoms?email=${context.authed_email}`)
+      .then((response) => {
+        setGraphdata(response.data);
+      }).catch(function (error) {
+        console.log(error);
+      });
+    //on get response
+  }, []);
 
   const axes = React.useMemo(
     () => [
@@ -49,8 +61,8 @@ const TrendPage = () => {
           margin: 'auto'
         }}
       >
-        {graphdata.length === 0 && <p style={{textAlign:'center'}}>No symptom data can be found.</p>}
-        {graphdata.length !== 0 && <> <Chart data={data} series={series} axes={axes} /> <p style={{textAlign:'center',margin:'2rem'}}>Your last day health score is 2. You are getting better.</p></>}
+        {graphdata.length === 0 && <p style={{ textAlign: 'center' }}>No symptom data can be found.</p>}
+        {graphdata.length !== 0 && <> <Chart data={data} series={series} axes={axes} /> <p style={{ textAlign: 'center', margin: '2rem' }}>Your last day health score is 2. You are getting better.</p></>}
       </div>
     </div>
   )
